b9fe87306a2540e1a596d449a1bdd57a71a77ebf
irdamdp <mirdadapollo@gmail.com>
2025-08-06 15:03:17 +0300

    Removed backend files from frontend repo

--- route/answerroute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { postanswer, getanswer } = require("../controller/answercontroller");
-
-router.post("/question", postanswer);
-router.get("/question/:questionid", getanswer);
-
-module.exports = router;
