5298caa701942beeda1e1a080894517f09557ad4
developer ab <contactdeveloperab@gmail.com>
2025-08-07 23:52:09 +0300

    setting up baseurls for deployments

--- client/src/axiosconfig.js
+++ client/src/axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosbase = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Use the environment variable for the base URL for deployment purpose
+  baseURL: import.meta.env.VITE_API_URL + "/api", // Use the environment variable for the base URL for deployment purpose
   // baseURL: "http://localhost:5500/api", this one for localhost
 });
 
